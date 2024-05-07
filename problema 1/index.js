082c870988dc2033b181b181183c3ca87dd01994
buggyes <matheusthja1@gmail.com>
2024-05-07 08:41:29 -0300

    making the http requests

---
+++ problema 1/index.js
@@ -0,0 +1,76 @@
+const url = 'https://dummy.restapiexample.com/api/v1/';
+var editing = false;
+
+document.addEventListener('DOMContentLoaded', function(){
+    initEmployeeTable();
+})
+
+async function initEmployeeTable(){
+
+}
+
+async function getAllEmployees(){
+    try{
+    const response = await fetch(url+'employees',{
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        redirect:"follow",
+        referrerPolicy:"no-referrer"
+    });
+    return response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function getEmployee(id){
+
+}
+
+async function postEmployee(data = {}){
+    try{
+        const response = await fetch(url+'create',{
+            method:"POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data)
+        })
+        return response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function putEmployee(id, data={}){
+    
+}
+
+async function deleteEmployee(id){
+    try{
+        const response = await fetch(url+"employee/"+id, {
+            method: "DELETE",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer"
+        });
+        return response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
