0d2a3ae12ac42d6ec2a8e140e6deb6445a48f445
Buggyes <matheusthja1@gmail.com>
2024-05-14 14:19:07 -0300

    Finished Stylizing Problem 1

--- problema1/index.js
+++ problema1/index.js
@@ -3,6 +3,6 @@ function login(){
     let password = document.getElementById("passwordInput").value;
 
     if(username == 'admin' && password == 123){
-        document.location.replace('http://127.0.0.1:5500/problema%201/management.html');
+        document.location.replace('http://127.0.0.1:5500/management.html');
     }
 }
